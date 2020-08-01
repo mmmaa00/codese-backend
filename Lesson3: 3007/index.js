e456a58811f51352cde76224d74acdf0be0414e0
mmmaa00 <68803045+mmmaa00@users.noreply.github.com>
2020-08-01 22:35:18 +0700

    Create index.js

---
+++ Lesson3: 3007/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { request, response } = require('express');
+const pool = require('./utils//db');
+const app = express() ;
+
+app.get('/users',(request,response) => {
+    pool.query('select * from `user`;',(err,data) => {
+        if(err) console.log(err);
+        console.log(data);
+        response.send(data)
+    })
+})
+app.get('/products',(request,response) => {
+    pool.query('select * from `product`;',(err,data) => {
+        if(err) console.log(err);
+        console.log(data);
+        response.send(data)
+    })
+})
+app.get('/orders',(request,response) => {
+    pool.query('select * from `order`;',(err,data) => {
+        if(err) console.log(err);
+        console.log(data);
+        response.send(data)
+    })
+})
+app.get('/categories',(request,response) => {
+    pool.query('select * from `category`;',(err,data) => {
+        if(err) console.log(err);
+        console.log(data);
+        response.send(data)
+    })
+})
+const callback = () => {
+    console.log('Running at 8080');
+}
+//localhost:8080/
+app.listen(8080, callback)
