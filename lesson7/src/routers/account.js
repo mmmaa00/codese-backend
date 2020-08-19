a7fd0a7b3aac410aed46c3ae9459f8a25de1dc2e
mmmaa00 <68803045+mmmaa00@users.noreply.github.com>
2020-08-19 21:32:44 +0700

    Create account.js

---
+++ lesson7/src/routers/account.js
@@ -0,0 +1,10 @@
+const accountController = require('../controllers/account');
+const Route = require('express').Router();
+
+Route.get('/',accountController.getAllAccount);
+Route.get('/:id',accountController.getAccountById);
+Route.post('/:',accountController.createAccount);
+Route.put('/:id',accountController.updateAccountById);
+Route.delete('/:id',accountController.deleteAccountById);
+
+module.exports = Route;
